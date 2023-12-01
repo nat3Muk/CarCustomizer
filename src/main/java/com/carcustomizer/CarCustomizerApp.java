5e445b5b2c899464886c87986e27f07d54aa0e00
nat3Muk <nathan.mukooba@colordao.edu>
2023-12-01 15:21:42 -0700

    CarModel Menu Look Up

--- src/main/java/com/carcustomizer/CarCustomizerApp.java
+++ src/main/java/com/carcustomizer/CarCustomizerApp.java
@@ -2,7 +2,7 @@ package main.java.com.carcustomizer;
 
 import main.java.com.carcustomizer.ui.ConsoleUI;
 import main.java.com.carcustomizer.ui.InputHandler;
-
+import main.java.com.carcustomizer.models.*;
 public class CarCustomizerApp {
     public static void main(String[] args) {
         ConsoleUI ui = new ConsoleUI();
@@ -33,6 +33,9 @@ public class CarCustomizerApp {
             case 2:
                 // Customize a Car logic
                 System.out.println("BUILD YOUR DREAM MODEL CAR");
+                CarModel carModels = new CarModel();
+                carModels.readCSV("/Users/nathanmukooba/Documents/cu_boulder20-24/Fall_2023_CUBoulder/CSCi4448-OOP/Project6n7/final-project-nat3Muk/src/main/java/com/carcustomizer/models/csvs/2020Models.csv");
+                carModels.printCarModels();
                 break;
             case 3:
                 System.out.println("Exiting the application. Goodbye!");
--- src/main/java/com/carcustomizer/models/CarModel.java
+++ src/main/java/com/carcustomizer/models/CarModel.java
@@ -1,59 +1,71 @@
 package main.java.com.carcustomizer.models;
-// import com.opencsv.CSVReader;
-// import com.opencsv.exceptions.CsvValidationException;
 
-// import java.io.FileReader;
-// import java.io.IOException;
-// import java.util.ArrayList;
-// import java.util.HashMap;
-// import java.util.List;
-// import java.util.Map;
-
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.StringTokenizer;
 
 public class CarModel {
-    // private Map<String, List<CarModel>> carModelsByMake;
+    private Map<String, List<String>> carModels;
 
-    // public CarModel() {
-    //     carModelsByMake = new HashMap<>();
+    public CarModel() {
+        carModels = new HashMap<>();
+    }
+
+    public void readCSV(String filePath) {
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            boolean firstLine = true;
+
+            while ((line = br.readLine()) != null) {
+                if (firstLine) {
+                    firstLine = false;
+                    continue; // Skip the header line
+                }
+                parseCSVLine(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void parseCSVLine(String line) {
+        StringTokenizer tokenizer = new StringTokenizer(line, ",");
+        List<String> values = new ArrayList<>();
+
+        // Skip the first token which contains the year
+        tokenizer.nextToken();
+
+        while (tokenizer.hasMoreTokens()) {
+            String token = tokenizer.nextToken().replaceAll("\"", "").trim();
+            values.add(token);
+        }
+
+        String make = values.get(0);
+        values.remove(0);
+
+        if (carModels.containsKey(make)) {
+            carModels.get(make).addAll(values);
+        } else {
+            carModels.put(make, values);
+        }
+    }
+
+    public void printCarModels() {
+        for (Map.Entry<String, List<String>> entry : carModels.entrySet()) {
+            System.out.println("Make: " + entry.getKey());
+            System.out.println("Models: " + entry.getValue());
+            System.out.println();
+        }
+    }
+
+    // public static void main(String[] args) {
+    //     CarModel carModels = new CarModel();
+    //     carModels.readCSV("path/to/your/file.csv");
+    //     carModels.printCarModels();
     // }
-
-    // public void readCSV(String filePath) {
-    //     try (CSVReader reader = new CSVReader(new FileReader(filePath))) {
-    //         String[] line;
-    //         reader.readNext(); // Skip header line
-    //         while ((line = reader.readNext()) != null) {
-    //             int year = Integer.parseInt(line[0]);
-    //             String make = line[1];
-    //             String model = line[2];
-    //             String[] bodyStyles = line[3].replaceAll("[\\[\\]\"]", "").split(", ");
-
-    //             CarModel carModel = new CarModel();
-    //             carModel.setYear(year);
-    //             carModel.setMake(make);
-    //             carModel.setModel(model);
-    //             carModel.setBodyStyles(bodyStyles);
-
-    //             carModelsByMake.computeIfAbsent(make, k -> new ArrayList<>()).add(carModel);
-    //         }
-    //     } catch (IOException | CsvValidationException e) {
-    //         e.printStackTrace();
-    //     }
-    // }
-
-    // public void displayModelsByMake(String make) {
-    //     List<CarModel> models = carModelsByMake.get(make);
-    //     if (models != null) {
-    //         System.out.println("Available models for " + make + ":");
-    //         for (CarModel model : models) {
-    //             System.out.println("- " + model.getModel() + " (" + model.getYear() + ")");
-    //         }
-    //     } else {
-    //         System.out.println("No models found for " + make);
-    //     }
-    // }
-
-    // Other methods and functionalities related to car models
-    // ...
 }
-    
-
