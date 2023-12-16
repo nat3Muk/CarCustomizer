cf067df05c7647d0108bcb802f09a237d7e3c622
Abiriham1 <95562687+Abiriham1@users.noreply.github.com>
2023-12-15 19:50:59 -0700

    Update CarSaveObserver.java

--- src/main/java/com/carcustomizer/services/CarSaveObserver.java
+++ src/main/java/com/carcustomizer/services/CarSaveObserver.java
@@ -3,18 +3,26 @@ package main.java.com.carcustomizer.services;
 import java.io.FileWriter;
 import java.io.IOException;
 
+// CustomizationObserver implementation that saves customized car details to a file
 public class CarSaveObserver implements CustomizationObserver {
+
     @Override
+    // Updates the observer with the latest car customization details
     public void update(Car car) {
         try {
+            // FileWriter is used to write to the file
             FileWriter fileWriter = new FileWriter("customized_car.txt", true); // Append mode
+
+            // Writing car details to the file
             fileWriter.write("Car Make: " + car.getMake() + "\n"); // You need to have getter methods for these properties
             fileWriter.write("Car Model: " + car.getModel() + "\n");
             fileWriter.write("Color: " + car.getColor() + "\n");
             fileWriter.write("Wheel Type: " + car.getWheelType() + "\n");
             fileWriter.write("Interior: " + car.getInterior() + "\n");
             fileWriter.write("Sound System: " + car.getSoundSystem() + "\n");
+
             fileWriter.close();
+            //checks for errors
         } catch (IOException e) {
             System.out.println("An error occurred while saving the customized car details.");
             e.printStackTrace();
