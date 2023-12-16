261efa18b89b47a367423c4e2160f6f6ff24602b
Abiriham1 <95562687+Abiriham1@users.noreply.github.com>
2023-12-15 19:52:12 -0700

    Update CustomizationObserver.java

--- src/main/java/com/carcustomizer/services/CustomizationObserver.java
+++ src/main/java/com/carcustomizer/services/CustomizationObserver.java
@@ -1,5 +1,7 @@
 package main.java.com.carcustomizer.services;
 
 public interface CustomizationObserver {
+        // Method to update the observer with the latest car customization details
+
     void update(Car car);
 }
