6675be2e06ad20adf4e900045be7475ba1fd00bf
nat3Muk <nathan.mukooba@colordao.edu>
2023-12-15 19:42:29 -0700

    Updates

--- src/main/java/com/carcustomizer/ui/CarCustomizerManager.java
+++ src/main/java/com/carcustomizer/ui/CarCustomizerManager.java
@@ -14,96 +14,97 @@ import java.util.List;
 import java.util.Scanner;
 
 public class CarCustomizerManager {
-    private ConsoleUI ui;
-    private InputHandler input;
-    private Car car;
-    private CarFactory carFactory;  // Added CarFactory
+    // Declaring necessary variables
+    private ConsoleUI ui; // User interface
+    private InputHandler input; // Input handler
+    private Car car; // Car instance
+    private CarFactory carFactory; // Car factory instance
 
-    private CarModel carModels;
-    private Scanner scanner;
+    private CarModel carModels; // Car models
+    private Scanner scanner; // Scanner for user input
 
-    private String selectedMake;
-    private String selectedModel;
-    private String selectedBodyType;
+    private String selectedMake; // Selected car make
+    private String selectedModel; // Selected car model
+    private String selectedBodyType; // Selected car body type
 
+    // Constructor initializing various components
+    public CarCustomizerManager() {
+        this.carFactory = new CarFactory(); // Instantiating CarFactory
+        this.ui = new ConsoleUI(); // Instantiating ConsoleUI
+        this.input = new InputHandler(); // Instantiating InputHandler
+        this.car = new Car(); // Instantiating Car
+        this.carModels = new CarModel(); // Instantiating CarModel
+        this.carModels.readCSV("main/java/com/carcustomizer/models/csvs/2020Models.csv"); // Reading CSV for car models
+        this.scanner = new Scanner(System.in); // Initializing Scanner object for user input
+    }
 
-public CarCustomizerManager() {
-    this.carFactory = new CarFactory();
-    this.ui = new ConsoleUI();
-    this.input = new InputHandler();
-    this.car = new Car();
-    this.carModels = new CarModel(); // Instantiate CarModel
-    this.carModels.readCSV("main/java/com/carcustomizer/models/csvs/2020Models.csv"); // Read CSV here
-    this.scanner = new Scanner(System.in); // Initialize the Scanner object
-}
-
+    // Method to start the car customization process
     public void start() {
         boolean running = true;
         while (running) {
-            ui.displayMainMenu();
-            int choice = input.getUserChoice(1, 3);
+            ui.displayMainMenu(); // Displaying main menu
+            int choice = input.getUserChoice(1, 3); // Getting user choice
             switch (choice) {
                 case 1:
-                    viewCarOptions();
+                    viewCarOptions(); // Viewing available car options
                     break;
                 case 2:
-                    selectCarInCatalog();
-                    ui.displayFinalizedDetails(selectedMake, selectedModel, selectedBodyType, car);; // Display the finalized car details
+                    selectCarInCatalog(); // Selecting a car from the catalog and customizing
+                    ui.displayFinalizedDetails(selectedMake, selectedModel, selectedBodyType, car);
                     System.out.println("Do you want to save this customized car? (y/n)");
                     String saveChoice = scanner.nextLine();
                     if (saveChoice.equalsIgnoreCase("y")) {
-                        saveCustomizedCar(); // Save the customized car details
+                        saveCustomizedCar(); // Saving customized car details
                     }
                     break;
-                // ... other cases for saving, loading, exiting
                 case 3:
-                    System.out.println("Exiting program...");
-                    running = false; // This will exit the while loop and end the program
+                    System.out.println("Exiting program..."); // Exiting the program
+                    running = false; // Exiting the while loop to end the program
                     break;
                 default:
-                    System.out.println("Invalid choice.");
+                    System.out.println("Invalid choice."); // Handling invalid choices
                     break;
             }
         }
     }
 
-private void viewCarOptions() {
-    carModels.printCarModelsByYear(2020);
-    // Wait for user input to continue
-    System.out.println("Press Enter to return to the main menu...");
-    scanner.nextLine();
-}
+    // Method to view available car options
+    private void viewCarOptions() {
+        carModels.printCarModelsByYear(2020); // Printing car models for the year 2020
+        System.out.println("Press Enter to return to the main menu..."); // Prompting user to return to the main menu
+        scanner.nextLine(); // Waiting for user input to continue
+    }
 
-
-
+    // Method to customize the selected car
     private void customizeCar() {
         // Check if car make, model, and body type are selected
         if (selectedMake == null || selectedModel == null || selectedBodyType == null) {
             System.out.println("Please select a car make, model, and body type before customizing.");
-            return; // Exit the method
+            return; // Exiting the method if selections are incomplete
         }
 
+        // Getting user choices for wheel type, color, interior, and sound system
         WheelType wheelType = getWheelTypeChoice();
         Color color = getColorChoice();
         Interior interior = getInteriorChoice();
         SoundSystem soundSystem = getSoundSystemChoice();
 
-        // Set the properties on the car
+        // Setting properties on the car based on user choices
         car.setWheelType(wheelType);
         car.setColor(color);
         car.setInterior(interior);
         car.setSoundSystem(soundSystem);
 
-        // Debug output
+        // Debug output showing the chosen customizations
         System.out.println("Wheel Type: " + wheelType);
         System.out.println("Color: " + color);
         System.out.println("Interior: " + interior);
         System.out.println("Sound System: " + soundSystem);
 
-        // Set cost strategy and calculate cost
+        // Setting cost strategy and calculating cost for customization
         car.setCostStrategy(new BasicCostStrategy());
 
-        //Output to user what they have made.
+        // Outputting the details of the customized car to the user
         System.out.println("You have made a: " + selectedMake + " " + selectedModel + " " + selectedBodyType + "\t(ﾉ◕ヮ◕)ﾉ*:・ﾟ✧");
         System.out.println("With Custom:");
         System.out.println("Color: " + car.getColor());
@@ -111,59 +112,63 @@ private void viewCarOptions() {
         System.out.println("Interior: " + car.getInterior());
         System.out.println("Sound System: " + car.getSoundSystem());
 
-        car.attach(new CarSaveObserver());  // Attach an observer
+        car.attach(new CarSaveObserver()); // Attaching an observer for car customization
 
-        // Notify observers when customization is finalized
+        // Notifying observers when customization is finalized
         car.notifyAllObservers();
     }
 
-
-
+    // Method to select a car from the catalog
     private void selectCarInCatalog() {
+        // Selecting car make
         System.out.println("========Select a Car Make==========");
         carModels.printCarMakes();
         this.selectedMake = scanner.nextLine();
 
-
+        // Selecting car model
         System.out.println("[Select a Model for " + this.selectedMake + "]" + ":");
         carModels.printCarModelsByMake(this.selectedMake);
         this.selectedModel = scanner.nextLine();
 
+        // Selecting available body types for the chosen car model
         carModels.printBodyTypesByModel(this.selectedMake, this.selectedModel);
-
-        // Get available body types for the selected model
         List<String> availableBodyTypes = carModels.getBodyTypesByModel(this.selectedMake, this.selectedModel);
 
+        // Handling scenario where no body types are available for the selected model
         if (availableBodyTypes.isEmpty()) {
             System.out.println("No body types available for the selected model.");
             return;
         }
 
-        // Display available body types
+        // Displaying available body types for selection
         for (int i = 0; i < availableBodyTypes.size(); i++) {
             System.out.println((i + 1) + ". " + availableBodyTypes.get(i));
         }
 
+        // Getting user choice for body type
         int bodyTypeChoice = input.getUserChoice(1, availableBodyTypes.size());
         this.selectedBodyType = availableBodyTypes.get(bodyTypeChoice - 1);
         System.out.println("Body Type selected: " + this.selectedBodyType);
 
+        // Asking user if they want to customize the chosen car
         System.out.println("Do you want to customize this car? (y/n)");
         String customizeChoice = scanner.nextLine();
         if (customizeChoice.equalsIgnoreCase("y")) {
-            customizeCar(); // First, customize the car
+            customizeCar(); // Customizing the car
 
-            // Display the finalized car details, including the cost
+            // Displaying the finalized car details, including cost
             ui.displayFinalizedDetails(selectedMake, selectedModel, selectedBodyType, car);
 
-            // Ask the user if they want to save the customized car
+            // Asking the user if they want to save the customized car
             System.out.println("Do you want to save this customized car? (y/n)");
             String saveChoice = scanner.nextLine();
             if (saveChoice.equalsIgnoreCase("y")) {
-                saveCustomizedCar(); // Save the customized car details
+                saveCustomizedCar(); // Saving the customized car details
             }
         }
     }
+
+
 
     private WheelType getWheelTypeChoice() {
         System.out.println("Select Wheel Type:");
@@ -242,7 +247,7 @@ private void viewCarOptions() {
     }
 
 
-    //Method for deleteing saved List<> of customized cars when user presses 3. Exit Program
+
 }
 
 
