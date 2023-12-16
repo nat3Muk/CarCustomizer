47205352ce29cb7f10ee8fbe41874ed220957cd4
Abiriham1 <95562687+Abiriham1@users.noreply.github.com>
2023-12-15 19:51:45 -0700

    Update CustomizationCostStrategy.java

--- src/main/java/com/carcustomizer/services/CustomizationCostStrategy.java
+++ src/main/java/com/carcustomizer/services/CustomizationCostStrategy.java
@@ -1,5 +1,7 @@
 package main.java.com.carcustomizer.services;
 
 public interface CustomizationCostStrategy {
+// Method to calculate the customization cost for a given car
+
     double calculateCost(Car car);
 }
